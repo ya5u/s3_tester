934e365d4ddff66e8df421cca1d6919cea499814
Yasutaka Shinzaki <shinzaki@yasu26.tech>
2019-02-05 00:54:00 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const S3 = require('aws-sdk/clients/s3');
+const parse = require('csv-parse/lib/sync');
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+let credentials = 'credentials.csv';
+let region = 'ap-northeast-1';
+let Bucket = 'tekcubtset';
+let Key = 'testkey'
+let file = 'test.text';
+
+const argv = process.argv;
+const argv_len = argv.length;
+for (let i=0; i<argv_len; i++) {
+  switch (argv[i]) {
+    case '--credentials':
+      credentials = argv[i+1];
+      break;
+    case '--region':
+      region = arg[i+1];
+      break;
+    case '--bucket':
+      Bucket = arg[i+1];
+      break;
+    case '--key':
+      Key = arg[i+1];
+      break;
+    case '--file':
+      file = arg[i+1];
+      break;
+  }
+}
+
+async function main() {
+  let cred;
+  try {
+    const csv = await readFile(credentials, { encoding: 'utf8' });
+    cred = parse(csv, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  const s3 = new S3({
+    apiVersion: '2006-03-01',
+    accessKeyId: cred['Access key ID'],
+    secretAccessKey: cred['Secret access key'],
+  });
+
+  let Body;
+  try {
+    Body = await readFile(file);
+  } catch (err) {
+    console.log(err);
+  }
+
+  const params = {
+    Bucket,
+    Key,
+    Body,
+  };
+
+  try {
+    const res = await s3.putObject(params).promise();
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
